403582a2c71e3f94037f84cdaf7c050040d0b112
TheHolyException <36442683+TheHolyException@users.noreply.github.com>
2021-12-10 18:54:03 +0100

    Just... dont ask...

---
+++ src/de/theholyexception/holyapi/util/exceptions/MalakaAreYouDumbException.java
@@ -0,0 +1,10 @@
+package de.theholyexception.holyapi.util.exceptions;
+
+public class MalakaAreYouDumbException extends RuntimeException {
+
+	private static final long serialVersionUID = -8780554325752297089L;
+
+	public MalakaAreYouDumbException(String message) {
+		super(message);
+	}
+}
